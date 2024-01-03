c7aff48b5d511fd22b225406a0ef52a0f8dc7ab9
bhavy111111 <31708192+bhavy111111@users.noreply.github.com>
2024-01-03 05:41:38 -0800

    Display FoodItems by ReverseLookup(Prefetch in marketplace_views.py)

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from vendor.models import Vendor
-
+from menu.models import Category,FoodItem
+from django.db.models import Prefetch
 def marketplace(request):
     vendors=Vendor.objects.filter(is_approved=True , user__is_active=True)
     vendors_count=vendors.count()
@@ -13,7 +14,14 @@ def marketplace(request):
 def vendor_detail(request,vendor_slug):
 
     vendor = get_object_or_404(Vendor,vendor_slug = vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset = FoodItem.objects.filter(is_available=True)
+        )
+    )
     context={
         'vendor':vendor,
+        'categories':categories,
     }
     return render(request,'marketplace/vendor_detail.html',context)
