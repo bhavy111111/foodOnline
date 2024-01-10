6475ee5d6e7d316abe830bbae9f24e82f93329e2
bhavy111111 <31708192+bhavy111111@users.noreply.github.com>
2024-01-10 06:01:40 -0800

    marketplace views search according to location

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404,HttpResponse
+from django.shortcuts import render,get_object_or_404,HttpResponse,redirect
 from vendor.models import Vendor
 from menu.models import Category,FoodItem
 from django.db.models import Prefetch
@@ -6,6 +6,9 @@ from django.http import JsonResponse
 from .models import Cart
 from .context_processors import get_cart_counter,get_cart_amount
 from django.db.models import Q
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D  # ``D`` is a shortcut for ``Distance``
+from django.contrib.gis.db.models.functions import Distance
 
 def marketplace(request):
     vendors=Vendor.objects.filter(is_approved=True , user__is_active=True)
@@ -115,30 +118,45 @@ def cart(request):
     return render(request,'marketplace/cart.html',context)
 
 def search(request):
-    address=request.GET['address']
-    #print('Address coming from home.html',address)
-    latitude = request.GET['lat']
-    longitude = request.GET['long']
-    radius = request.GET['radius']
-    keyword=request.GET['keyword']
+    if not  'address' in  request.GET:
+        return redirect('marketplace')
+    else:
+        address=request.GET['address']
+        #print('Address coming from home.html',address)
+        latitude = request.GET['lat']
+        longitude = request.GET['long']
+        radius = request.GET['radius']
+        keyword=request.GET['keyword']
 
 
-    print(address,latitude,longitude,radius,keyword)
-    
-    #GET VENDOR IDS THAT HAS THE FOOD USER IS LOOKING FOR
-    fetch_food_items = FoodItem.objects.filter(food_title__icontains=keyword,is_available=True).values_list('vendor',flat=True)
-    #print(fetch_food_items)
-    
-    vendors = Vendor.objects.filter(Q(id__in=fetch_food_items)| Q(vendor_name__icontains=keyword , is_approved=True,user__is_active=True))
-    print(vendors)
-    #cat
-    #vendors =Vendor.objects.filter(vendor_name__icontains=keyword , is_approved=True,user__is_active=True)
-    print('Search',vendors)
-    vendors_count=vendors.count()
-    
-    context={
-        'vendors':vendors,
-        'vendors_count':vendors_count,
-        'fetch_food_item':fetch_food_items,
-    }
-    return render (request,'marketplace/listings.html',context)
+        print(address,latitude,longitude,radius,keyword)
+        
+        #GET VENDOR IDS THAT HAS THE FOOD USER IS LOOKING FOR
+        fetch_food_items = FoodItem.objects.filter(food_title__icontains=keyword,is_available=True).values_list('vendor',flat=True)
+        #print(fetch_food_items)
+        
+        vendors = Vendor.objects.filter(Q(id__in=fetch_food_items)| Q(vendor_name__icontains=keyword , is_approved=True,user__is_active=True))
+        print(vendors)
+        #gdal configuration
+        if(latitude and longitude and radius):
+            #IN point long will be first
+            pnt = GEOSGeometry('POINT(%s %s)'%(longitude,latitude))
+            vendors = Vendor.objects.filter(Q(id__in=fetch_food_items)| Q(vendor_name__icontains=keyword , is_approved=True,user__is_active=True),
+            user_profile__location__distance_lte=(pnt, D(km=radius))
+            ).annotate(distance=Distance('user_profile__location',pnt)).order_by('distance')
+
+        for v in vendors:
+            v.kms = round(v.distance.km)
+
+        #cat
+        #vendors =Vendor.objects.filter(vendor_name__icontains=keyword , is_approved=True,user__is_active=True)
+        print('Search',vendors)
+        vendors_count=vendors.count()
+        
+        context={
+            'vendors':vendors,
+            'vendors_count':vendors_count,
+            'fetch_food_item':fetch_food_items,
+            'address':address,
+        }
+        return render (request,'marketplace/listings.html',context)
