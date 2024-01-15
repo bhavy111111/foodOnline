2fa1345d63869d8d337aced6e169324ffb7f17fa
bhavy111111 <31708192+bhavy111111@users.noreply.github.com>
2024-01-15 04:45:35 -0800

    Set the location in session varibales in navbar.html and home views

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -5,17 +5,29 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D  # ``D`` is a shortcut for ``Distance``
 from django.contrib.gis.db.models.functions import Distance
 
-def home(request):
-    if 'lat' in request.GET:
-        print('if test start from here ')
+def get_or_set_current_location(request):
+    # if lat and long are stored in session will take session variable
+    if 'lat' in request.session:
+        lat=request.session['lat']
+        lng=request.session['lng']
+        return lng,lat
+    #if lat and long are stored in browser will take browser variable
+    elif 'lat' in request.GET:
         lat = request.GET.get('lat')
         lng = request.GET.get('lng')
 
-        print(lat,lng)
+        # if they are in url variable , it will make them session variables
+        request.session['lat']=lat
+        request.session['lng']=lng
+        return lng,lat
+    else:
+        return None
 
 
-                #IN point lng will be first
-        pnt = GEOSGeometry('POINT(%s %s)'%(lng,lat))
+def home(request):
+    if get_or_set_current_location(request) is not None:
+               #IN point lng will be first
+        pnt = GEOSGeometry('POINT(%s %s)'%(get_or_set_current_location(request)))
 
         # Query that bring nearby restro to home page
         vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance('user_profile__location',pnt)).order_by('distance')
